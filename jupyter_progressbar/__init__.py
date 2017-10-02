6cf19091629a2a06dc4d4ea6a313f6732ad57a68
Herbert Kruitbosch <H.T.Kruitbosch@rug.nl>
2017-10-02 12:33:08 +0200

    more textual info

--- jupyter_progressbar/__init__.py
+++ jupyter_progressbar/__init__.py
@@ -5,17 +5,22 @@ import humanize
 from math import sqrt
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 def ProgressBar(iter, size=None):
     progress = widgets.FloatProgress(value=0.0, min=0.0, max=1.0)
     text = widgets.HTML(
-        value="<b>0</b>% <b>0</b>s passed",
+        value="<b>0</b>s passed",
         placeholder='0%',
         description='',
     )
-    display(widgets.HBox([progress, text]))
+    text2 = widgets.HTML(
+        value="<b>0</b>% or <b>0</b> of <b>0</b> done",
+        placeholder='0%',
+        description='',
+    )
+    display(widgets.VBox([widgets.HBox([progress, text]), text2]))
     if size is None and hasattr(iter, '__len__'):
         size = len(iter)
     if size is None:
@@ -34,6 +39,8 @@ def ProgressBar(iter, size=None):
         progress.value = p
         std = humanize.naturaldelta((size - i) * sqrt((tsq / i) - (t / i) ** 2))
         left = humanize.naturaldelta((1 - p) / p * t if p > 0 else 0)
-        text.value = "<b>{}</b>% <b>{}</b> &plusmn; <b>{}</b> left".format(round(100 * p), left, std)
-
+        text.value = "<b>{}</b> &plusmn; <b>{}</b> left".format(left, std)
+        text2.value = "<b>{}</b>% or <b>{}</b> of <b>{}</b> done".format(round(100 * p), i, size)
+    text.value = ''
+    text2.value = "took <b>{}</b> to process <b>{}</b> items".format(humanize.naturaldelta(t), i)
     progress.value = 100
